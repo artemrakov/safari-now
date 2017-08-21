a87170d41ab5dce953059c84d6a862affaec3096
Artem Rakov <scoff1k@gmail.com>
2017-08-21 16:20:16 +0100

    added safaris controllers methods

---
+++ app/controllers/safaris_controller.rb
@@ -0,0 +1,30 @@
+class SafarisController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
