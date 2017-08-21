67491f8af721a3fcced5d01da893ff81412f9fa2
Artem Rakov <scoff1k@gmail.com>
2017-08-21 17:32:41 +0100

    added safari photoes to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821143726) do
+ActiveRecord::Schema.define(version: 20170821162742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170821143726) do
     t.string   "date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.json     "photos"
     t.index ["user_id"], name: "index_safaris_on_user_id", using: :btree
   end
 
