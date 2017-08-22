978384af335030ef8e8ce8f77815de0587cf513c
James Aung <therealaung@gmail.com>
2017-08-22 12:09:57 +0100

    added seed for 20 users and added auto photo mounting with carrierwave

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,32 @@
-User.destroy_all
-Safari.destroy_all
-Booking.destroy_all
+require 'faker'
+# User.destroy_all
+# Safari.destroy_all
+# Booking.destroy_all
 
-user_1 = User.create(name: "hello", email: "hello@lewagon.com", password: "123456")
+# safari = Safari.new(title: "Safari")
+# safari.user = user_1
+# safari.save
 
-user_2 = User.create(name: "hi", email: "hi@lewagon.com", password: "123456")
+# book = Booking.new
+# book.safari = safari
+# book.user = user_2
+# book.save
 
-safari = Safari.new(title: "Safari")
-safari.user = user_1
-safari.save
+user_pic_array = [ "http://xdesktopwallpapers.com/wp-content/uploads/2012/07/Scott%20Porter%20Looking%20At%20Camera%20And%20White%20Background.jpg",
+"https://profile.microsoft.com/RegsysProfileCenter/Images/personal_info.jpg",
+"https://profile.microsoft.com/RegsysProfileCenter/Images/contact_pref.jpg",
+"http://paccpronetwork.com/wp-content/uploads/2015/09/athlete.png",
+"http://www.runnersworld.com/sites/runnersworld.com/files/styles/article_main_image_2200px/public/articles/2015/08/nicksymmondsbrookssinglet_1.jpg?itok=PDdyfNq_",
+"https://media.aws.iaaf.org/media/Medium/b6b16b8e-68d3-453b-9191-196ac8550227.png?v=-2003354641",
+"https://usatftw.files.wordpress.com/2014/10/ap_singapore_tennis__wta_finals_68191523.jpg?w=1000&h=600&crop=1",
+"http://www.sportsmanias.com/images/gallery/27-100-hottest-instagram/display/81-Maggie-Vessey.jpg"]
 
-book = Booking.new
-book.safari = safari
-book.user = user_2
-book.save
+
+20.times do
+  counter = 1
+  photo_url = user_pic_array.sample
+  name = Faker::GameOfThrones.character
+  user = User.create(name: name, email: Faker::Internet.email(name.split(" ").first), description: Faker::Lorem.paragraph,  password: "password", remote_avatar_url: photo_url)
+  puts "Create user number #{counter}"
+  counter += 1
+end
