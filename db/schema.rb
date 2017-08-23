ca1cc1a3fd1eba6c49d509379829390ddfce73d8
Chiara Davies <chiaradavies01@outlook.com>
2017-08-23 10:53:46 +0100

    changed the name of the col location to address and edited the seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822172049) do
+ActiveRecord::Schema.define(version: 20170823094636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20170822172049) do
 
   create_table "safaris", force: :cascade do |t|
     t.string   "title"
-    t.string   "location"
+    t.string   "address"
     t.integer  "price"
     t.text     "description"
     t.integer  "capacity"
