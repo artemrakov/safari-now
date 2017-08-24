9527d52c97b2432367817f79216ac6beab9504fd
James Aung <therealaung@gmail.com>
2017-08-24 17:32:14 +0100

    changed avatar to cloudinary zoom on face

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,10 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  process eager: true  # Force version generation at upload time.
+
+  version :bright_face do
+    cloudinary_transformation effect: "brightness:10", radius: 20,
+      width: 150, height: 150, crop: :thumb, gravity: :face
+  end
 end
