4e96fcc1bd8a83f26799dd7372dce38e261dd524
James Aung <therealaung@gmail.com>
2017-08-25 11:35:18 +0100

    added extra inputs to new user devise form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :avatar, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :avatar, :avatar_cache, :email, :password, :password_confirmation)
   end
 
   def account_update_params
