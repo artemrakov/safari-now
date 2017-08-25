92ce2f8bb7da6bddc897bdd9803b9ef1b81464ab
Chiara Davies <chiaradavies01@outlook.com>
2017-08-25 16:00:24 +0100

    reviews can be added and are displayd with stars on the show page with the reviewers name

--- app/controllers/safaris_controller.rb
+++ app/controllers/safaris_controller.rb
@@ -20,6 +20,7 @@ class SafarisController < ApplicationController
 
    def show
     @review = Review.new
+    @reviews = Review.all
     @safari = Safari.find(params[:id])
     @safari_coordinates = { lat: @safari.latitude, lng: @safari.longitude }
     @user = @safari.user
