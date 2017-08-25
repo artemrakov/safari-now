81dfa82baf1b24c6422759be2d631e28a43f244a
James Aung <therealaung@gmail.com>
2017-08-25 11:32:08 +0100

    added registration controller to add custom fields to edit profile. added custom fields to edit form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
   post 'create_booking/:safari_id/:id', to: 'pages#create_booking', as: 'create_booking'
